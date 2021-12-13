a5db2b7c8b6221684e3956d67b908c0ca0070ef3
Shaun Saker <sakershaun@gmail.com>
2021-12-13 17:10:28 +0200

    feature/toasted icon

--- src/components/CountryFlag.tsx
+++ src/components/CountryFlag.tsx
@@ -1,10 +1,8 @@
 import React, { ReactElement } from 'react';
 import ReactCountryFlag from 'react-country-flag';
-import { styled, theme } from '../styles/stitches.config';
+import { SmallIconContainer } from './SmallIconContainer';
 
-const CONTAINER_SIZE = 32;
-
-const FLAG_SIZE = CONTAINER_SIZE * 1.5;
+const FLAG_SIZE = 48;
 
 interface CountryFlagProps {
   countryCode: string;
@@ -14,24 +12,12 @@ export const CountryFlag = ({
   countryCode,
 }: CountryFlagProps): ReactElement => {
   return (
-    <Container>
+    <SmallIconContainer>
       <ReactCountryFlag
         countryCode={countryCode}
         svg
         style={{ width: FLAG_SIZE, height: FLAG_SIZE }}
       />
-    </Container>
+    </SmallIconContainer>
   );
 };
-
-const Container = styled('div', {
-  width: CONTAINER_SIZE,
-  height: CONTAINER_SIZE,
-  borderRadius: CONTAINER_SIZE / 2,
-  backgroundColor: theme.colors.white,
-  display: 'flex',
-  justifyContent: 'center',
-  alignItems: 'center',
-  overflow: 'hidden',
-  border: `2px solid ${theme.colors.transWhite}`,
-});
--- src/components/IngredientCard.tsx
+++ src/components/IngredientCard.tsx
@@ -3,6 +3,11 @@ import { RecipeIngredient } from '../recipes/models';
 import { Card } from './Card';
 import ingredientsData from '../data/ingredients.json';
 import { Ingredient } from '../ingredients/models';
+import { SmallIconContainer } from './SmallIconContainer';
+import FireIcon from './icons/fire.svg';
+import { theme } from '../styles/stitches.config';
+
+const FIRE_ICON_SIZE = 20;
 
 interface IngredientCardProps extends RecipeIngredient {}
 
@@ -17,6 +22,17 @@ export const IngredientCard = ({
     <Card
       title={ingredient.title}
       subtitle={`${quantity} ${ingredient.unit}`}
+      cornerComponent={
+        toasted && (
+          <SmallIconContainer>
+            <FireIcon
+              width={FIRE_ICON_SIZE}
+              height={FIRE_ICON_SIZE}
+              fill={theme.colors.accent.toString()}
+            />
+          </SmallIconContainer>
+        )
+      }
     />
   );
 };
